98f9f09e0aadfba3f6a4f63747a14ea59087afd3
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2023-12-28 22:30:28 +0530

    Add files via upload

---
+++ DataTypesVariables/dataType.java
@@ -0,0 +1,32 @@
+class dataType{
+public static void main(String[] args)
+{
+boolean var;
+var=true;
+char a;
+a='@';
+byte b;
+b=-5;
+short c;
+c=24;
+int i;
+i=15;
+long l;
+l=1000;
+float f;
+f=0.4f;
+double d;
+d=100.5d;
+String num="012345";
+System.out.println(var);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(num);
+
+}
+}
