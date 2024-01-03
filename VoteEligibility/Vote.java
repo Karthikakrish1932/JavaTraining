66f34f88cc2f4522729748e07bde2291e83a5523
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-01-03 22:24:22 +0530

    Add files via upload

---
+++ VoteEligibility/Vote.java
@@ -0,0 +1,10 @@
+class Vote{
+public static void main(String[] args){
+int VotingAge=18;
+int inputAge=22;
+if(inputAge>=VotingAge)
+  System.out.print("Eligible");
+else
+  System.out.print("NOT Eligible");
+}
+}
