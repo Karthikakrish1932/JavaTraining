13f4ea4fbb0fc65e9af2c01a3eaf1157e965a57c
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-01-10 23:06:09 +0530

    Add files via upload

---
+++ Multiplication Tables/Multiplication tables using do-while loop/Tables2.java
@@ -0,0 +1,12 @@
+class Tables2
+{
+  public static void main (String[]args)
+  {
+    int n = 4;
+    int i=1;
+    do{
+	System.out.println(n + "*" + i + "=" + n*i);
+        i++;
+      }while(i<=10);
+  }
+}
---
+++ Multiplication Tables/Multiplication tables using while loop/Tables1.java
@@ -0,0 +1,13 @@
+class Tables1
+{
+  public static void main (String[]args)
+  {
+    int n = 4;
+    int i=1;
+    while(i <= 10)
+      {
+	System.out.println(n + "*" + i + "=" + n*i);
+        i++;
+      }
+  }
+}
