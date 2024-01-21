4c7f7fc5ad7364a2bfa29950a878ac7318bc489f
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-01-21 23:54:14 +0530

    Add files via upload

---
+++ Fibonacci series/Fibonacci.java
@@ -0,0 +1,21 @@
+class Fibonacci
+{
+  public static void main (String[]args)
+  {
+    int n = 3;
+    int a=1,b=2,c;
+    System.out.println (a);
+    System.out.println (b);
+   
+    do
+      {
+        c = a + b;
+	System.out.println (c);
+	a = b;
+	b = c;
+	n++;
+      }
+    while (n <=7);
+
+  }
+}
