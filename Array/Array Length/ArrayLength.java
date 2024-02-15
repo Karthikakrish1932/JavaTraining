bb3b500e18e246441b73d62e415536aa4c718d80
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-15 21:48:29 +0530

    Add files via upload

---
+++ Array/Array Length/ArrayLength.java
@@ -0,0 +1,9 @@
+class ArrayLength
+{
+public static void main(String[] args)
+{
+int numbers[]=new int[]{1, 2, 3, 4, 5};
+int length=numbers.length;
+System.out.println("Array Length:"+length);
+}
+}
