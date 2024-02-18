3fdac8c67969e5661057cb0c538960d104b68784
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-18 14:53:00 +0530

    Add files via upload

---
+++ Variables and Datatypes/Checking Even/CheckingEven.java
@@ -0,0 +1,11 @@
+public class CheckingEven{
+public static void main(String[]args){
+int num=7;
+boolean isEven;
+if(num%2==0)
+isEven=true;
+else
+isEven=false;
+System.out.println("Is num even: "+isEven);
+}
+}
