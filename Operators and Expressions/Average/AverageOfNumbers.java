120aff01f8425aa2bdebeb81ce2dbc28a777735d
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-18 15:52:42 +0530

    Add files via upload

---
+++ Operators and Expressions/Average/AverageOfNumbers.java
@@ -0,0 +1,8 @@
+public class AverageOfNumbers{
+public static void main(String[]args){
+int num1=15;
+int num2=10;
+double average=(num1+num2)/2;
+System.out.println("The average is: "+average);
+}
+}
