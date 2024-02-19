ba74247b3c6892c00b5d8f5a623c0d5ecfe8f317
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-19 19:45:55 +0530

    Add files via upload

---
+++ Control Statements/Maximum of three numbers/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+public static void main(String[]args){
+int a=10,b=5,c=50;
+if(a>b && a>c)
+{
+System.out.println("a is big");
+}
+else if(b>a && b>c)
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("c is big");
+}
+}
+}
