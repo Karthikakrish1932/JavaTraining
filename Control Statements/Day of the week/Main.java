27daccc2da5f2f5c43e9193d87f0c47b5f549e63
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-19 22:42:42 +0530

    Add files via upload

---
+++ Control Statements/Day of the week/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+public static void main(String[]args)
+{
+  int number=2;
+  switch(number)
+  {
+  case 1:
+          System.out.println("Day of the week: Monday");
+          break;
+  case 2:
+          System.out.println("Day of the week: Tuesday");
+          break;
+  case 3:
+          System.out.println("Day of the week: Wednesday");
+          break;
+  case 4:
+          System.out.println("Day of the week: Thursday");
+          break;
+  case 5:
+          System.out.println("Day of the week: Friday");
+          break;
+  case 6:
+          System.out.println("Day of the week: Saturday");
+          break;
+   case 7:
+          System.out.println("Day of the week: Sunday");
+          break;
+  default:
+          System.out.println("Invalid day number");
+          break;
+  }
+}
+}
