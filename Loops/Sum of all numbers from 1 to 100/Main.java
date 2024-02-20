46972ce39687033d4cade154534d250567e0b931
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-20 09:43:59 +0530

    Add files via upload

---
+++ Loops/Sum of all numbers from 1 to 100/Main.java
@@ -0,0 +1,16 @@
+class Main
+{
+  public static void main (String[]args){
+  int sum=0;
+  for(int i=1;i<=100;i++)
+  {
+  sum+=i;
+  }
+  System.out.println("Sum of all numbers from 1 to 100: "+sum);
+}
+}
+
+
+  
+
+  
