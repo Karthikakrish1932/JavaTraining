456bd03725c32c0bf3d15fa9b7101650a2c07f07
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-02-25 14:11:19 +0530

    Add files via upload

---
+++ Methods/Method with Return Value/Main.java
@@ -0,0 +1,12 @@
+class Main{
+public static boolean isEven(int a){
+    if(a%2==0)
+    {
+    return true;
+    }
+return false;
+}
+public static void main(String[]args){
+System.out.println("a is even: "+isEven(19));
+}
+}
