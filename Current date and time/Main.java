b518ba5f2e3098f3d77fb4afb59010954982d4a4
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-03-01 19:49:41 +0530

    Add files via upload
    Current date and time

---
+++ Current date and time/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+public static void main(String[] args) {
+    Date currentDateAndTime = new Date();
+    System.out.println("Current Date and Time: " + currentDateAndTime);
+   }
+}
