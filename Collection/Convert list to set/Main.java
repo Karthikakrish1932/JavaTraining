04413696e8d5725b09aca394b01cefe5911d05f7
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-03-05 23:04:04 +0530

    Add files via upload

---
+++ Collection/Convert list to set/Main.java
@@ -0,0 +1,20 @@
+/*Implement a Java program that converts a list to a set and vice versa. 
+Initialize an ArrayList with integers and convert it into a HashSet. 
+Then, convert the HashSet back into an ArrayList. Print both the list and the set after each conversion.*/
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        Set<Integer> set1 = new HashSet<>(list1);
+        System.out.println("ArrayList to HashSet");
+        System.out.println(set1);
+        List<Integer> list2 = new ArrayList<>(set1);
+        System.out.println("HashSet to ArrayList");
+        System.out.println(list2);
+        }
+}
