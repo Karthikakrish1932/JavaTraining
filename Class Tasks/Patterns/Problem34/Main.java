2bc26e8bd1976a8ad7d75d90b3e2a91692ad5596
Karthikakrish1932 <155040094+Karthikakrish1932@users.noreply.github.com>
2024-04-04 00:00:07 +0530

    Add files via upload

---
+++ Class Tasks/Patterns/Problem34/Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+public static void main(String[] args) {
+int n=7; 
+int letter=65;
+for(int i=1;i<=n;i++)
+{
+for(int k=1;k<=n-i;k++)
+{
+System.out.print("  ");
+}
+for(int j=0;j<i;j++)
+{
+System.out.print((char)(letter+j)+" ");
+}
+System.out.println();
+}
+for(int i=1;i<=n-1;i++)
+{
+for(int k=1;k<=i;k++)
+{
+System.out.print("  ");
+}
+for(int j=0;j<n-i;j++)
+{
+System.out.print((char)(letter+j)+" ");
+}
+System.out.println();
+}
+}
+}
